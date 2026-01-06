ff68a1edc11e4db9d2dc47f31333ef48841a792f
lisb-org <lisger.lg@gmail.com>
2026-01-06 15:47:59 +0100

    github

---
+++ game/places/ce.ts
@@ -0,0 +1,3 @@
+place.get('slot_1').onClick(() => {
+    game.navigate('__splash__')
+})
---
+++ game/places/circular_economy.ts
@@ -0,0 +1,11 @@
+place.get('slot_1').onClick(() => {
+    game.navigate('throw_away')
+})
+
+place.get('industry').onClick(() => {
+    window.open("https://youtu.be/9N5Kv7yfHxg?si=LX6I9bOX1tphFtJZ");
+});
+
+place.get('ce').onClick(() => {
+    game.navigate('ce')
+})
